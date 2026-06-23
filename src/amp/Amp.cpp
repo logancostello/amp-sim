989804022b67a02aa132b849728b47a61e2a7a31
logancostello <locostel@calpoly.edu>
2026-06-22 19:33:01 -0700

    add: improved gain with 3 layers of gain and distortion

--- src/amp/Amp.cpp
+++ src/amp/Amp.cpp
@@ -8,6 +8,12 @@ float Amp::processInput(float input, AmpSettings* ampSettings) {
     float distorted = input * ampSettings->gain;
     distorted = std::tanh(distorted);
 
+    distorted = input * ampSettings->gain;
+    distorted = std::tanh(distorted);
+
+    distorted = input * ampSettings->gain;
+    distorted = std::tanh(distorted);
+
     float output = distorted * ampSettings->volume;
     return output;
 }
